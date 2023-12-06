3414caef60bc62ee58c56d0981f3ca01a9cd7023
runic-dev <contact.runicdev@gmail.com>
2023-12-06 16:25:22 +0100

    Adds ability to parse ids

--- aoc_day_02/src/lib.rs
+++ aoc_day_02/src/lib.rs
@@ -1,13 +1,26 @@
 fn parse_color_amount(input: String) -> Vec<(usize, String)> {
-    input.split(", ").map(|color_amount_str| {
-        let (amount, color) = color_amount_str.split_once(' ').unwrap();
-        (amount.parse::<usize>().unwrap(), color.to_string())
-    }).collect::<Vec<(usize, String)>>()
+    input
+        .split(", ")
+        .map(|color_amount_str| {
+            let (amount, color) = color_amount_str.split_once(' ').unwrap();
+            (amount.parse::<usize>().unwrap(), color.to_string())
+        })
+        .collect::<Vec<(usize, String)>>()
+}
+
+fn parse_game_id(input: String) -> usize {
+    input
+        .split(' ')
+        .last()
+        .expect("Invalid format for Game Id info")
+        .parse::<usize>()
+        .unwrap()
 }
 
 #[cfg(test)]
 pub mod test {
     use super::parse_color_amount;
+    use super::parse_game_id;
     use rstest::rstest;
 
     #[rstest]
@@ -20,7 +33,10 @@ pub mod test {
         (9, "blue".to_string()),
         (22, "green".to_string())
     ])]
-    fn parse_color_and_amount_from_string(#[case] input: String, #[case] expected: Vec<(usize, String)>) {
+    fn parse_color_and_amount_from_string(
+        #[case] input: String,
+        #[case] expected: Vec<(usize, String)>,
+    ) {
         let parsed_info: Vec<(usize, String)> = parse_color_amount(input);
         let first_tuple = parsed_info.first().unwrap();
         let last_tuple = parsed_info.last().unwrap();
@@ -28,6 +44,17 @@ pub mod test {
         assert_eq!(first_tuple.1, expected.first().unwrap().1);
         assert_eq!(last_tuple.0, expected.last().unwrap().0);
         assert_eq!(last_tuple.1, expected.last().unwrap().1);
+    }
+
+    #[rstest]
+    #[case("Game 1", 1)]
+    #[case("Game 2", 2)]
+    #[case("Game 3", 3)]
+    #[case("Game 45", 45)]
+    #[case("Game 59", 59)]
+    fn parse_game_ids_from_string(#[case] input: String, #[case] expected: usize) {
+        let result = parse_game_id(input);
+        assert_eq!(result, expected);
     }
 
     #[test]
